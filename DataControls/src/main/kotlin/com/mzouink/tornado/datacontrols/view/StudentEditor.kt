fb91107d0b8538621410e25883b2909494ebf771
Marwan <zouinkhi.marwan@gmail.com>
2020-05-06 14:19:07 +0200

    full use of ViewModel connection, validation and save

--- DataControls/src/main/kotlin/com/mzouink/tornado/datacontrols/view/StudentEditor.kt
+++ DataControls/src/main/kotlin/com/mzouink/tornado/datacontrols/view/StudentEditor.kt
@@ -3,38 +3,49 @@ package com.mzouink.tornado.datacontrols.view
 import com.mzouink.tornado.datacontrols.controllers.MainController
 import com.mzouink.tornado.datacontrols.model.Student
 import com.mzouink.tornado.datacontrols.model.StudentModel
-import javafx.beans.property.SimpleObjectProperty
-import javafx.beans.property.SimpleStringProperty
 import tornadofx.*
-import java.time.LocalDate
 
 class StudentEditor : View("My View") {
     val mainController: MainController by inject()
 
-    val model : StudentModel by inject()
+    //    val firstname = SimpleStringProperty()
+//    val lastname = SimpleStringProperty()
+//    val dob = SimpleObjectProperty<LocalDate>()
+    val model: StudentModel by inject()
 
     override val root = form {
         fieldset {
             field("First Name") {
-                textfield(model.firstName) { }
-            }
-            field("Last Name") {
-                textfield(model.lastName) { }
-            }
-            field("Date of Birth") {
-                datepicker(model.birthday) { }
-            }
-        }
-        button("Save") {
-            action {
-                if (model.firstName.value.isNullOrEmpty() || model.lastName.value.isNullOrEmpty()) {
-
-                } else {
-                    val student = Student(1, model.firstName.value, model.lastName.value, model.birthday.value)
-                    mainController.addStudent(student)
+                textfield(model.firstName) {
+                    required()
+//                    or use
+//                    validator {
+//                        if(it.isNullOrBlank()) error("This is required") else null
+//                    }
                 }
             }
-        }
+            field("Last Name") {
+                textfield(model.lastName).required()
+            }
+            field("Date of Birth") {
 
+                datepicker(model.birthday).required()
+            }
+        }
+        hbox {
+            button("Save") {
+                enableWhen(model.dirty)
+                action {
+                    model.commit {
+                        val student = Student(1, model.firstName.value, model.lastName.value, model.birthday.value)
+                        mainController.addStudent(student)
+                    }
+                }
+            }
+            button("Reset") {
+                enableWhen(model.dirty)
+                action { model.rollback() }
+            }
+        }
     }
 }
